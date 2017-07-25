98087a5f42896f4f81df030cf176e0695e73c9d7
David King <david@oodavid.com>
2017-07-25 17:05:45 +0100

    Demo > Absolute URLs for images

--- static/demo/angularjs-meta-tags-management/app.js
+++ static/demo/angularjs-meta-tags-management/app.js
@@ -53,20 +53,25 @@ function MetaTagsService(){
 
 
 
-function ArticleService(){
+ArticleService.$inject = ['$location'];
+function ArticleService($location){
+  var rootUrl = $location.protocol()+'://'+$location.host();
+  if($location.port()){
+    rootUrl += ':'+$location.port();
+  }
   var service = this;
   this.getArticle = getArticle;
   var articles = {
     'one': {
       title: 'AngularJS Meta Tags Management',
       description: 'How to manage and update meta tags in your AngularJS app. This AngularJS service lets you manage the <meta> tags used by various social networks and search engines. From Google to Facebook and Twitter to Pinterest.',
-      image: '/demo/angularjs-meta-tags-management/meta-tags.png',
+      image: rootUrl+'/demo/angularjs-meta-tags-management/meta-tags.png',
       twitter_id: '@oodavid',
     },
     'two': {
       title: 'AngularJS Pagination Component',
       description: 'A simple component that adds pagination to your pages. The debounce feature is especially useful for rapid cycling of pages without overloading your server.',
-      image: '/demo/angularjs-meta-tags-management/pagination.png',
+      image: rootUrl+'/demo/angularjs-meta-tags-management/pagination.png',
       twitter_id: '@oodavid',
     },
   }
